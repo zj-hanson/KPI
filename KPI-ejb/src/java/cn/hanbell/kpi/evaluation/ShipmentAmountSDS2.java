5478303a5f7901e9047842652f1b5f1f45f6fb18
DongTianyu <dtianyu@hanbell.cn>
2018-07-18 17:20:58 +0800

    出货金额不含ARM235

--- KPI-ejb/src/java/cn/hanbell/kpi/evaluation/ShipmentAmountSDS2.java
+++ KPI-ejb/src/java/cn/hanbell/kpi/evaluation/ShipmentAmountSDS2.java
@@ -14,12 +14,12 @@ public class ShipmentAmountSDS2 extends ShipmentAmount {
     public ShipmentAmountSDS2() {
         super();
         queryParams.put("facno", "C");
-        queryParams.put("deptno", " '1G500' ");
+        queryParams.put("deptno", " '1T100' ");
         queryParams.put("decode", "2");
         queryParams.put("ogdkid", "RL03");
         queryParams.put("n_code_DA", "='AH' ");
         queryParams.put("n_code_DC", " ='SDS'");
-        queryParams.put("n_code_DD", " ='00' ");
+        queryParams.put("n_code_DD", " in ('00','02') ");
     }
 
 }
