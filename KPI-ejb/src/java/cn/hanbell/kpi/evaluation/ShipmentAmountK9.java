ebf44d75b3444a2bad0e7360498946928889c959
C1879 <40564060+C1879@users.noreply.github.com>
2018-07-31 14:31:49 +0800

    柯茂收费服务计算接口

---
+++ KPI-ejb/src/java/cn/hanbell/kpi/evaluation/ShipmentAmountK9.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cn.hanbell.kpi.evaluation;
+
+/**
+ *
+ * @author C1879
+ */
+public class ShipmentAmountK9 extends ShipmentAmount9 {
+
+    public ShipmentAmountK9() {
+        super();
+        queryParams.put("facno", "K");
+        //queryParams.put("decode", "");
+        queryParams.put("n_code_DA", " IN('OH','RT') ");
+    }
+
+}
