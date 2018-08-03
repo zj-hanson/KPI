25b7e9a8d75767b4008c87e2afce481b756c0358
C1879 <40564060+C1879@users.noreply.github.com>
2018-08-03 14:18:17 +0800

    外销R制冷订单计算接口增加判断条件

--- KPI-ejb/src/java/cn/hanbell/kpi/evaluation/SalesOrderQuantityR1T1.java
+++ KPI-ejb/src/java/cn/hanbell/kpi/evaluation/SalesOrderQuantityR1T1.java
@@ -17,6 +17,7 @@ public class SalesOrderQuantityR1T1 extends SalesOrderQuantity{
         queryParams.put("deptno", " '1T000','1T100' ");
         queryParams.put("decode", "2");
         queryParams.put("n_code_DA", " ='R' ");
+        queryParams.put("n_code_CD", "  LIKE 'WX%' ");
         queryParams.put("n_code_DC", " ='R' ");
         queryParams.put("n_code_DD", " ='00' ");
     }
