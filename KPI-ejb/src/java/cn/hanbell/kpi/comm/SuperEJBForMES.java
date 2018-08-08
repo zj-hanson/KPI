4a0faf6dc054630a090932f2f971668779ab8fa8
C1749 <huanglang@hanbell.cn>
2018-08-08 13:06:27 +0800

    底层CRM连接代码修改
    底层CRM连接代码修改

--- KPI-ejb/src/java/cn/hanbell/kpi/comm/SuperEJBForMES.java
+++ KPI-ejb/src/java/cn/hanbell/kpi/comm/SuperEJBForMES.java
@@ -6,33 +6,29 @@
 package cn.hanbell.kpi.comm;
 
 import com.lightshell.comm.SuperEJB;
+import java.io.Serializable;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 /**
  *
  * @author C0160
- * @param <T>
  */
-public abstract class SuperEJBForMES<T> extends SuperEJB<T> {
+@Stateless
+@LocalBean
+public class SuperEJBForMES implements Serializable {
 
     @PersistenceContext(unitName = "CRM-ejbPU")
     private EntityManager em;
 
-    public SuperEJBForMES(Class<T> entityClass) {
-        super(entityClass);
+    public SuperEJBForMES() {
+
     }
 
-    @Override
     public EntityManager getEntityManager() {
         return em;
-    }
-
-    public String formatString(String value, String format) {
-        if (value.length() >= format.length()) {
-            return value;
-        }
-        return format.substring(0, format.length() - value.length()) + value;
     }
 
 }
