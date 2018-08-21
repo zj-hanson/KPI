c12c7cb954662433bd712e6cb01a6f162d67b317
C1879 <40564060+C1879@users.noreply.github.com>
2018-08-21 17:14:15 +0800

    刀具耗用实绩管理表计算方法及计算接口增加

---
+++ KPI-ejb/src/java/cn/hanbell/kpi/evaluation/ConsumptionCutterYX.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cn.hanbell.kpi.evaluation;
+
+import cn.hanbell.kpi.ejb.IndicatorBean;
+import cn.hanbell.kpi.entity.Indicator;
+import cn.hanbell.kpi.entity.IndicatorDetail;
+import java.lang.reflect.Field;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+/**
+ *
+ * @author C1879
+ */
+public class ConsumptionCutterYX extends ConsumptionCutter {
+
+    IndicatorBean indicatorBean = lookupIndicatorBeanBean();
+
+    public ConsumptionCutterYX() {
+        super();
+        queryParams.put("formid", "A-圆型件刀具耗用");
+        queryParams.put("deptno", "1P500");
+    }
+
+    //得到Other1值与Other5的值相加
+    @Override
+    public BigDecimal getValue(int y, int m, Date d, int type, LinkedHashMap<String, Object> map) {
+        String mon;
+        Field f;
+        BigDecimal v1;
+        Double a1, a6;
+        Indicator i = indicatorBean.findByFormidYearAndDeptno(map.get("formid").toString(), y, map.get("deptno").toString());
+        IndicatorDetail o1 = i.getOther1Indicator();
+        IndicatorDetail o6 = i.getOther6Indicator();
+        try {
+            mon = indicatorBean.getIndicatorColumn("N", m);
+            f = o1.getClass().getDeclaredField(mon);
+            f.setAccessible(true);
+            a1 = Double.valueOf(f.get(o1).toString());
+
+            f = o6.getClass().getDeclaredField(mon);
+            f.setAccessible(true);
+            a6 = Double.valueOf(f.get(o6).toString());
+
+            v1 = BigDecimal.valueOf(a6 / a1);
+
+            return v1;
+        } catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException ex) {
+            Logger.getLogger(ProcessQuantityHFX.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return BigDecimal.ZERO;
+    }
+
+    private IndicatorBean lookupIndicatorBeanBean() {
+        try {
+            Context c = new InitialContext();
+            return (IndicatorBean) c.lookup("java:global/KPI/KPI-ejb/IndicatorBean!cn.hanbell.kpi.ejb.IndicatorBean");
+        } catch (NamingException ne) {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, "exception caught", ne);
+            throw new RuntimeException(ne);
+        }
+    }
+}
