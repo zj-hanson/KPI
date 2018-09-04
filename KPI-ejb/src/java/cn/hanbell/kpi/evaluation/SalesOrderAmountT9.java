2b63749a9c17dabd8c90fdf03ac84f7c0d59df10
C1879 <40564060+C1879@users.noreply.github.com>
2018-09-04 17:47:08 +0800

    国际营销课外销零件订单金额计算接口修改

--- KPI-ejb/src/java/cn/hanbell/kpi/evaluation/SalesOrderAmountT9.java
+++ KPI-ejb/src/java/cn/hanbell/kpi/evaluation/SalesOrderAmountT9.java
@@ -5,6 +5,14 @@
  */
 package cn.hanbell.kpi.evaluation;
 
+import com.lightshell.comm.BaseLib;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.persistence.Query;
+
 /**
  *
  * @author C1879
@@ -16,7 +24,66 @@ public class SalesOrderAmountT9 extends SalesOrderAmount {
         queryParams.put("facno", "C");
         queryParams.put("decode", "2");
         queryParams.put("n_code_DA", " IN ('AA','AH') ");
-        queryParams.put("n_code_CD", " NOT LIKE '%TW%' ");
+        queryParams.put("n_code_CD", " LIKE 'WX%' ");
         queryParams.put("n_code_DD", " ='01' ");//00是整机-01是零件-02是后处理
     }
+
+    @Override
+    public BigDecimal getValue(int y, int m, Date d, int type, LinkedHashMap<String, Object> map) {
+         //获得查询参数
+        String facno = map.get("facno") != null ? map.get("facno").toString() : "";
+        String decode = map.get("decode") != null ? map.get("decode").toString() : "";
+        String n_code_DA = map.get("n_code_DA") != null ? map.get("n_code_DA").toString() : "";
+        String n_code_CD = map.get("n_code_CD") != null ? map.get("n_code_CD").toString() : "";
+        String n_code_DC = map.get("n_code_DC") != null ? map.get("n_code_DC").toString() : "";
+        String n_code_DD = map.get("n_code_DD") != null ? map.get("n_code_DD").toString() : "";
+
+        BigDecimal tram = BigDecimal.ZERO;
+        StringBuilder sb = new StringBuilder();
+        sb.append(" SELECT  isnull(convert(decimal(16,2),sum((d.tramts*h.ratio)/(h.taxrate+1))),0) from cdrdmas d inner join cdrhmas h on h.facno=d.facno and h.cdrno=d.cdrno");
+        sb.append(" WHERE  isnull(h.hmark2,'') <> 'FW' AND h.hrecsta <> 'W' and h.cusno NOT IN ('SSD00107','SGD00088','SJS00254','SCQ00146','STW00003') and h.depno='1T100' ");
+        sb.append(" AND  h.facno='${facno}' ");
+        if (!"".equals(decode)) {
+            sb.append(" and h.decode ='").append(decode).append("' ");
+        }
+        if (!"".equals(n_code_DA)) {
+            sb.append(" and d.n_code_DA ").append(n_code_DA);
+        }
+        if (!"".equals(n_code_CD)) {
+            sb.append(" and d.n_code_CD ").append(n_code_CD);
+        }
+        if (!"".equals(n_code_DC)) {
+            sb.append(" and d.n_code_DC ").append(n_code_DC);
+        }
+        if (!"".equals(n_code_DD)) {
+            sb.append(" and d.n_code_DD ").append(n_code_DD);
+        }
+        sb.append(" and year(h.recdate) = ${y} and month(h.recdate)= ${m} ");
+        switch (type) {
+            case 2:
+                //月
+                sb.append(" and h.recdate<= '${d}' ");
+                break;
+            case 5:
+                //日
+                sb.append(" and h.recdate= '${d}' ");
+                break;
+            default:
+                sb.append(" and h.recdate<= '${d}' ");
+        }
+        String cdrdmas = sb.toString().replace("${y}", String.valueOf(y)).replace("${m}", String.valueOf(m)).replace("${d}", BaseLib.formatDate("yyyyMMdd", d))
+                .replace("${facno}", facno);
+
+        superEJB.setCompany(facno);
+        Query query1 = superEJB.getEntityManager().createNativeQuery(cdrdmas);
+        try {
+            Object o1 = query1.getSingleResult();
+            tram = (BigDecimal) o1;
+        } catch (Exception ex) {
+            Logger.getLogger(Shipment.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return tram;
+    }
+    
+    
 }
