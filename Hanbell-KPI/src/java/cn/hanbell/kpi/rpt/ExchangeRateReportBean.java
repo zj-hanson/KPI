60767ec9e5cd37b12824859af97c58b282c8ea48
C1879 <C1879@hanbell.cn>
2018-10-20 15:43:20 +0800

    汇率走势图增加

---
+++ Hanbell-KPI/src/java/cn/hanbell/kpi/rpt/ExchangeRateReportBean.java
@@ -0,0 +1,291 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cn.hanbell.kpi.rpt;
+
+import cn.hanbell.kpi.control.UserManagedBean;
+import cn.hanbell.kpi.ejb.ExchangeRateBean;
+import cn.hanbell.kpi.ejb.IndicatorChartBean;
+import cn.hanbell.kpi.entity.IndicatorChart;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Objects;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
+import org.primefaces.model.chart.Axis;
+import org.primefaces.model.chart.AxisType;
+import org.primefaces.model.chart.CategoryAxis;
+import org.primefaces.model.chart.ChartSeries;
+import org.primefaces.model.chart.LineChartModel;
+
+/**
+ *
+ * @author C1879
+ */
+@ManagedBean(name = "exchangeRateReportBean")
+@ViewScoped
+public class ExchangeRateReportBean implements Serializable {
+
+    @EJB
+    protected IndicatorChartBean indicatorChartBean;
+
+    @EJB
+    protected ExchangeRateBean exchangeRateBean;
+
+    //查询开始时间
+    protected Date queryDateBegin;
+    //查询结束时间
+    protected Date queryDateEnd;
+    //货币种类
+    protected String queryCurrency;
+    //div隐藏属性
+    protected String displaySting;
+    //提示
+    protected String remind;
+    //map为table显示内容
+    protected LinkedHashMap<String, String> map;
+    protected List<String[]> list;
+
+    protected LineChartModel chartModel;
+
+    @ManagedProperty(value = "#{userManagedBean}")
+    protected UserManagedBean userManagedBean;
+
+    FacesContext fc;
+    ExternalContext ec;
+    protected IndicatorChart indicatorChart;
+
+    public ExchangeRateReportBean() {
+    }
+
+    public Calendar rtndate() {
+        Calendar c = Calendar.getInstance();
+        c.setTime(getUserManagedBean().getBaseDate());
+        return c;
+    }
+
+    @PostConstruct
+    public void construct() {
+        displaySting = "none";
+        remind = "block";
+        fc = FacesContext.getCurrentInstance();
+        ec = fc.getExternalContext();
+        HttpServletRequest request = (HttpServletRequest) ec.getRequest();
+        String id = request.getParameter("id");
+        if (id == null) {
+            fc.getApplication().getNavigationHandler().handleNavigation(fc, null, "error");
+        }
+        indicatorChart = indicatorChartBean.findById(Integer.valueOf(id));
+        if (indicatorChart == null) {
+            fc.getApplication().getNavigationHandler().handleNavigation(fc, null, "error");
+        }
+
+        queryDateBegin = rtndate().getTime(); //new SimpleDateFormat("yyyy/MM/dd").format(c.getTime());
+        queryDateEnd = rtndate().getTime();
+
+        map = new LinkedHashMap<>();
+        list = new ArrayList<>();
+        chartModel= new LineChartModel();
+
+    }
+
+    public void initial() {
+        displaySting = "none";
+        remind = "block";
+        queryDateBegin = rtndate().getTime();
+        queryDateEnd = rtndate().getTime();
+        list = new ArrayList<>();
+        map = new LinkedHashMap<>();
+        queryCurrency = "0";
+        chartModel.clear();
+    }
+
+    public void queryExchangeRate() {
+        boolean aa = true;
+        map = new LinkedHashMap<>();
+        list.clear();
+        chartModel.clear();
+        displaySting = "none";
+        remind = "block";
+        if (queryDateBegin.after(queryDateEnd)) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", "查询开始日期不能大于结束日期！"));
+            aa = false;
+        }
+        if (queryDateEnd.after(rtndate().getTime())) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", "日期选择不能超过系统结算日期！"));
+            aa = false;
+        }
+        if (queryCurrency.equals("0")) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", "请选择需要查询的币种！"));
+            aa = false;
+        }
+        if (aa) {
+            map = exchangeRateBean.getHashMap(queryCurrency, queryDateBegin, queryDateEnd);
+            if (map != null) {
+                list = exchangeRateBean.getlList(queryCurrency, queryDateBegin, queryDateEnd);
+                ChartSeries t = new ChartSeries();
+                t.setLabel(map.get("name"));
+                for (int i = 0; i < list.size(); i++) {
+                    String[] get = list.get(i);
+                    t.set(get[0], Double.parseDouble(get[1]));
+                }
+                getChartModel().addSeries(t);
+                getChartModel().setLegendPosition("e");
+                //getChartModel().setTitle("");
+                getChartModel().setShowPointLabels(true);
+                getChartModel().setBreakOnNull(true);
+                getChartModel().setSeriesColors("28B732");//自定义颜色
+                Axis yAxis;
+                getChartModel().getAxes().put(AxisType.X, new CategoryAxis("日期"));
+                yAxis = getChartModel().getAxis(AxisType.Y);
+                //yAxis.setLabel("");
+                displaySting = "block";
+                remind = "none";
+            } else {
+                displaySting = "none";
+                remind = "block";
+                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", "无法查询到该日期的数据，请重新查询！"));
+            }
+        }
+    }
+
+    /**
+     * @return the queryDateBegin
+     */
+    public Date getQueryDateBegin() {
+        return queryDateBegin;
+    }
+
+    /**
+     * @return the queryDateEnd
+     */
+    public Date getQueryDateEnd() {
+        return queryDateEnd;
+    }
+
+    /**
+     * @return the queryCurrency
+     */
+    public String getQueryCurrency() {
+        return queryCurrency;
+    }
+
+    /**
+     * @param queryDateBegin the queryDateBegin to set
+     */
+    public void setQueryDateBegin(Date queryDateBegin) {
+        this.queryDateBegin = queryDateBegin;
+    }
+
+    /**
+     * @param queryDateEnd the queryDateEnd to set
+     */
+    public void setQueryDateEnd(Date queryDateEnd) {
+        this.queryDateEnd = queryDateEnd;
+    }
+
+    /**
+     * @param queryCurrency the queryCurrency to set
+     */
+    public void setQueryCurrency(String queryCurrency) {
+        this.queryCurrency = queryCurrency;
+    }
+
+    /**
+     * @return the displaySting
+     */
+    public String getDisplaySting() {
+        return displaySting;
+    }
+
+    /**
+     * @param displaySting the displaySting to set
+     */
+    public void setDisplaySting(String displaySting) {
+        this.displaySting = displaySting;
+    }
+
+    /**
+     * @return the map
+     */
+    public LinkedHashMap<String, String> getMap() {
+        return map;
+    }
+
+    /**
+     * @param map the map to set
+     */
+    public void setMap(LinkedHashMap<String, String> map) {
+        this.map = map;
+    }
+
+    /**
+     * @return the userManagedBean
+     */
+    public UserManagedBean getUserManagedBean() {
+        return userManagedBean;
+    }
+
+    /**
+     * @param userManagedBean the userManagedBean to set
+     */
+    public void setUserManagedBean(UserManagedBean userManagedBean) {
+        this.userManagedBean = userManagedBean;
+    }
+
+    /**
+     * @return the list
+     */
+    public List<String[]> getList() {
+        return list;
+    }
+
+    /**
+     * @param list the list to set
+     */
+    public void setList(List<String[]> list) {
+        this.list = list;
+    }
+
+    /**
+     * @return the remind
+     */
+    public String getRemind() {
+        return remind;
+    }
+
+    /**
+     * @param remind the remind to set
+     */
+    public void setRemind(String remind) {
+        this.remind = remind;
+    }
+
+    /**
+     * @return the chartModel
+     */
+    public LineChartModel getChartModel() {
+        return chartModel;
+    }
+
+    /**
+     * @param chartModel the chartModel to set
+     */
+    public void setChartModel(LineChartModel chartModel) {
+        this.chartModel = chartModel;
+    }
+
+}
---
+++ KPI-ejb/src/java/cn/hanbell/kpi/ejb/ExchangeRateBean.java
@@ -0,0 +1,275 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cn.hanbell.kpi.ejb;
+
+import cn.hanbell.kpi.comm.SuperEJBForERP;
+import com.lightshell.comm.BaseLib;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author C1879
+ */
+@Stateless
+@LocalBean
+public class ExchangeRateBean implements Serializable {
+
+    @EJB
+    private SuperEJBForERP erpEJB;
+
+    private final DecimalFormat df;
+    private final DecimalFormat doubledf;
+
+    public ExchangeRateBean() {
+        this.df = new DecimalFormat("0.####");
+        this.doubledf = new DecimalFormat("0.00％");
+    }
+
+    public List<String[]> getlList(String queryCurrency, Date queryDateBegin, Date queryDateEnd) {
+        List<String[]> list;
+        StringBuilder sb = new StringBuilder();
+        sb.append(" SELECT right(CONVERT(varchar(100), rateday, 111),5),rate FROM  arm_cw_rate where rpttype ='${queryCurrency}' ");
+        sb.append(" and rateday >= '${queryDateBegin}' and  rateday <= '${queryDateEnd}' ORDER BY rateday ASC ");
+        String sql = sb.toString().replace("${queryCurrency}", queryCurrency).replace("${queryDateBegin}", BaseLib.formatDate("yyyyMMdd", queryDateBegin)).replace("${queryDateEnd}", BaseLib.formatDate("yyyyMMdd", queryDateEnd));
+        try {
+            erpEJB.setCompany("C");
+            Query query = erpEJB.getEntityManager().createNativeQuery(sql);
+            List result = query.getResultList();
+            list = new ArrayList<>();
+            int size = 1;
+            //图表显示最多30条数据
+            while (((double)result.size() / (double)size) > 30) {
+                size = size * 2;
+            }
+            if (result != null && !result.isEmpty()) {
+                for (int i = 0; i < result.size(); i = i + size) {
+                    Object[] row = (Object[]) result.get(i);
+                    String[] arr = new String[2];
+                    arr[0] = row[0].toString();
+                    arr[1] = df.format(Double.parseDouble(row[1].toString()));
+                    list.add(arr);
+                    if ( i + size >= result.size()) {
+                        row = (Object[]) result.get(result.size() - 1);
+                        arr = new String[2];
+                        arr[0] = row[0].toString();
+                        arr[1] = df.format(Double.parseDouble(row[1].toString()));
+                        list.add(arr);
+                    }
+
+                }
+                return list;
+            }
+        } catch (Exception e) {
+            System.out.println("cn.hanbell.kpi.ejb.ExchangeRateBean.getlList()" + e);
+        }
+        return null;
+    }
+
+    public LinkedHashMap getHashMap(String queryCurrency, Date queryDateBegin, Date queryDateEnd) {
+        //标题兰name
+        String title;
+        //币种name
+        String name;
+        //环比时间
+        String HBtime;
+        //环比汇率
+        String HBrate;
+        //定比时间
+        String DBtime;
+        //定比汇率
+        String DBrate;
+        //环比、定比结束时间
+        String Endtime;
+        //环比、定比结束汇率
+        String Endrate;
+        //环比涨跌额
+        String HBamount;
+        //环比涨跌幅度
+        String HBrange;
+        //环比涨幅颜色；
+        String HBcolor;
+        //定比涨跌额
+        String DBamount;
+        //定比涨跌幅度
+        String DBrange;
+        //定比涨幅颜色
+        String DBcolor;
+        LinkedHashMap<String, String> map = new LinkedHashMap<>();
+
+        List<String> hbList = getDateBegin(queryCurrency, queryDateBegin, queryDateEnd);
+        List<String> endList = getDateEnd(queryCurrency, queryDateBegin, queryDateEnd);
+        List<String> dbList = getDBdate(queryCurrency, queryDateBegin);
+        if (hbList != null || endList != null) {
+            try {
+                title = getTitle(queryCurrency);
+                name = dbList.get(0);
+                DBtime = dbList.get(1);
+                DBrate = df.format(Double.valueOf(dbList.get(2)));
+                HBtime = hbList.get(0);
+                HBrate = df.format(Double.valueOf(hbList.get(1)));
+                Endtime = endList.get(0);
+                Endrate = df.format(Double.valueOf(endList.get(1)));
+                //环比
+                // 涨跌额=当前选定的结束日期的金额 — 当前选定的开始日期的金额
+                // 涨跌幅度=涨跌额/当前选定的开始日期的金额*100
+                HBamount = df.format(BigDecimal.valueOf(Double.parseDouble(Endrate)).subtract(BigDecimal.valueOf(Double.parseDouble(HBrate))));
+                HBrange = doubledf.format((Double.parseDouble(Endrate) - Double.parseDouble(HBrate)) / Double.parseDouble(HBrate) * 100);
+                HBcolor = (Double.parseDouble(Endrate) - Double.parseDouble(HBrate)) < 0 ? "green" : "red";
+                //定比
+                //涨跌额=当前选定结束日期的金额 — 当前年度第一天正常班制的的对应金额
+                //涨跌幅度=涨跌额 /  当前年度第一天正常班制的的对应金额*100
+                DBamount = df.format(BigDecimal.valueOf(Double.parseDouble(Endrate)).subtract(BigDecimal.valueOf(Double.parseDouble(DBrate))));
+                DBrange = doubledf.format((Double.parseDouble(Endrate) - Double.parseDouble(DBrate)) / Double.parseDouble(DBrate) * 100);
+                DBcolor = Double.parseDouble(Endrate) - Double.parseDouble(DBrate) < 0 ? "green" : "red";
+                map.put("title", title);
+                map.put("name", name);
+                map.put("DBtime", DBtime);
+                map.put("DBrate", DBrate);
+                map.put("DBamount", DBamount);
+                map.put("DBrange", DBrange);
+                map.put("HBtime", HBtime);
+                map.put("HBrate", HBrate);
+                map.put("HBamount", HBamount);
+                map.put("HBrange", HBrange);
+                map.put("Endtime", Endtime);
+                map.put("Endrate", Endrate);
+                map.put("HBcolor", HBcolor);
+                map.put("DBcolor", DBcolor);
+                return map;
+            } catch (Exception e) {
+                System.out.println("cn.hanbell.kpi.ejb.ExchangeRateBean.getHashMap():" + e);
+            }
+        }
+        return null;
+    }
+
+    //得出table标题
+    private String getTitle(String queryCurrency) {
+        String title;
+        switch (queryCurrency) {
+            case "1":
+                title = "美金兑换人民币汇率";
+                break;
+            case "2":
+                title = "欧元兑换人民币汇率";
+                break;
+            case "3":
+                title = "日元兑换人民币汇率";
+                break;
+            case "4":
+                title = "人民币兑换台币汇率";
+                break;
+            case "5":
+                title = "国际黄金价格走势";
+                break;
+            case "6":
+                title = "美金兑换日元汇率";
+                break;
+            case "7":
+                title = "欧元兑换美金汇率";
+                break;
+            case "8":
+                title = "美金兑换台币汇率";
+                break;
+            default:
+                title = "";
+        }
+        return title;
+    }
+
+    //得到定比兑换名称、定比开始时间、兑换率
+    private List<String> getDBdate(String queryCurrency, Date queryDateBegin) {
+        Calendar c = Calendar.getInstance();
+        c.setTime(queryDateBegin);
+        List<String> list = new ArrayList<>();
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(" SELECT top 1 exchange,CONVERT(varchar(100), rateday, 111),rate FROM  arm_cw_rate where rpttype ='${queryCurrency}' ");
+        sb.append(" and year(rateday) = ${y}  ORDER BY rateday ASC ");
+        String sql = sb.toString().replace("${queryCurrency}", queryCurrency).replace("${y}", String.valueOf(c.get(Calendar.YEAR)));
+        try {
+            erpEJB.setCompany("C");
+            Query query = erpEJB.getEntityManager().createNativeQuery(sql);
+            List result = query.getResultList();
+            if (result != null && !result.isEmpty()) {
+                for (int i = 0; i < result.size(); i++) {
+                    Object[] row = (Object[]) result.get(i);
+                    list.add(row[0].toString());
+                    list.add(row[1].toString());
+                    list.add(row[2].toString());
+                }
+                return list;
+            }
+        } catch (Exception e) {
+            System.out.println("cn.hanbell.kpi.ejb.ExchangeRateBean.getDBdate()" + e);
+        }
+        return null;
+    }
+
+    //得到环比、定比结束时间、兑换率
+    private List<String> getDateEnd(String queryCurrency, Date queryDateBegin, Date queryDateEnd) {
+        List<String> list = new ArrayList<>();
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(" SELECT top 1 CONVERT(varchar(100), rateday, 111),rate FROM  arm_cw_rate where rpttype ='${queryCurrency}' ");
+        sb.append(" and rateday >= '${queryDateBegin}' and  rateday <= '${queryDateEnd}'  ORDER BY rateday DESC ");
+        String sql = sb.toString().replace("${queryCurrency}", queryCurrency).replace("${queryDateBegin}", BaseLib.formatDate("yyyyMMdd", queryDateBegin)).replace("${queryDateEnd}", BaseLib.formatDate("yyyyMMdd", queryDateEnd));
+        try {
+            erpEJB.setCompany("C");
+            Query query = erpEJB.getEntityManager().createNativeQuery(sql);
+            List result = query.getResultList();
+            if (result != null && !result.isEmpty()) {
+                for (int i = 0; i < result.size(); i++) {
+                    Object[] row = (Object[]) result.get(i);
+                    list.add(row[0].toString());
+                    list.add(row[1].toString());
+                }
+                return list;
+            }
+        } catch (Exception e) {
+            System.out.println("cn.hanbell.kpi.ejb.ExchangeRateBean.getDateEnd()" + e);
+        }
+        return null;
+    }
+
+    //得到环比开始时间、兑换率
+    private List<String> getDateBegin(String queryCurrency, Date queryDateBegin, Date queryDateEnd) {
+        List<String> list = new ArrayList<>();
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(" SELECT top 1 CONVERT(varchar(100), rateday, 111),rate FROM  arm_cw_rate where rpttype ='${queryCurrency}' ");
+        sb.append(" and rateday >= '${queryDateBegin}' and  rateday <= '${queryDateEnd}'  ORDER BY rateday ASC ");
+        String sql = sb.toString().replace("${queryCurrency}", queryCurrency).replace("${queryDateBegin}", BaseLib.formatDate("yyyyMMdd", queryDateBegin)).replace("${queryDateEnd}", BaseLib.formatDate("yyyyMMdd", queryDateEnd));
+        try {
+            erpEJB.setCompany("C");
+            Query query = erpEJB.getEntityManager().createNativeQuery(sql);
+            List result = query.getResultList();
+            if (result != null && !result.isEmpty()) {
+                for (int i = 0; i < result.size(); i++) {
+                    Object[] row = (Object[]) result.get(i);
+                    list.add(row[0].toString());
+                    list.add(row[1].toString());
+                }
+                return list;
+            }
+        } catch (Exception e) {
+            System.out.println("cn.hanbell.kpi.ejb.ExchangeRateBean.getDateBegin()" + e);
+        }
+        return null;
+    }
+
+}
