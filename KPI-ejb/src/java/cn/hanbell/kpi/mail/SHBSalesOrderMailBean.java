49a99c52a4f4995121b6757542caa1db7f7c7ed9
C1879 <C1879@hanbell.cn>
2018-11-21 15:15:37 +0800

    SHB每日订单越南项移除

--- KPI-ejb/src/java/cn/hanbell/kpi/mail/SHBSalesOrderMailBean.java
+++ KPI-ejb/src/java/cn/hanbell/kpi/mail/SHBSalesOrderMailBean.java
@@ -44,8 +44,8 @@ public class SHBSalesOrderMailBean extends SalesOrderMail {
         sb.append(getQuantityTable());
         sb.append("<div class=\"tableTitle\">单位：万元</div>");
         sb.append(getAmountTable());
-        sb.append("<div class=\"tableTitle\">单位：百万越南盾</div>");
-        sb.append(getVHBAmountTable());
+//        sb.append("<div class=\"tableTitle\">单位：百万越南盾</div>");
+//        sb.append(getVHBAmountTable());
         return sb.toString();
     }
 
@@ -119,13 +119,13 @@ public class SHBSalesOrderMailBean extends SalesOrderMail {
             total.setName("涡旋订单台数");
             sb.append(getHtmlTableRow(total, y, m, d));
 
-            indicators.clear();
-            indicators = indicatorBean.findByCategoryAndYear("越南订单台数", y);
-            indicatorBean.getEntityManager().clear();
-            getHtmlTable(indicators, y, m, d, true);
-            total = getSumIndicator();
-            total.setName("越南订单台数");
-            sb.append(getHtmlTableRow(total, y, m, d));
+//            indicators.clear();
+//            indicators = indicatorBean.findByCategoryAndYear("越南订单台数", y);
+//            indicatorBean.getEntityManager().clear();
+//            getHtmlTable(indicators, y, m, d, true);
+//            total = getSumIndicator();
+//            total.setName("越南订单台数");
+//            sb.append(getHtmlTableRow(total, y, m, d));
 
             sb.append("</table></div>");
         } catch (Exception ex) {
@@ -255,35 +255,35 @@ public class SHBSalesOrderMailBean extends SalesOrderMail {
         return sb.toString();
     }
 
-    protected String getVHBAmountTable() {
-        StringBuilder sb = new StringBuilder();
-        Indicator total;
-        try {
-            sb.append("<div class=\"tbl\"><table width=\"100%\">");
-            sb.append("<tr><th rowspan=\"2\" colspan=\"1\">产品别</th><th rowspan=\"2\" colspan=\"1\">本日</th>");
-            sb.append("<th rowspan=\"1\" colspan=\"5\">本月</th><th rowspan=\"1\" colspan=\"5\">年累计</th>");
-            sb.append("<th rowspan=\"2\" colspan=\"1\">年度目标</th><th rowspan=\"2\" colspan=\"1\">年度达成率</th></tr>");
-            sb.append("<tr><th colspan=\"1\">实际</th><th colspan=\"1\">目标</th><th colspan=\"1\">达成率</th><th colspan=\"1\">去年同期</th><th colspan=\"1\">成长率</th>");
-            sb.append("<th colspan=\"1\">实际</th><th colspan=\"1\">目标</th><th colspan=\"1\">达成率</th><th colspan=\"1\">去年同期</th><th colspan=\"1\">成长率</th>");
-            sb.append("</tr>");
-            salesOrder = new SalesOrderAmount();
-
-            indicators.clear();
-            indicators = indicatorBean.findByCategoryAndYear("越南订单金额", y);
-            indicatorBean.getEntityManager().clear();
-            indicators.stream().forEach((i) -> {
-                indicatorBean.divideByRate(i, 2);
-            });
-            getHtmlTable(indicators, y, m, d, true);
-            total = getSumIndicator();
-            total.setName("越南订单金额");
-            sb.append(getHtmlTableRow(total, y, m, d));
-
-            sb.append("</table></div>");
-        } catch (Exception ex) {
-            return ex.toString();
-        }
-        return sb.toString();
-    }
+//    protected String getVHBAmountTable() {
+//        StringBuilder sb = new StringBuilder();
+//        Indicator total;
+//        try {
+//            sb.append("<div class=\"tbl\"><table width=\"100%\">");
+//            sb.append("<tr><th rowspan=\"2\" colspan=\"1\">产品别</th><th rowspan=\"2\" colspan=\"1\">本日</th>");
+//            sb.append("<th rowspan=\"1\" colspan=\"5\">本月</th><th rowspan=\"1\" colspan=\"5\">年累计</th>");
+//            sb.append("<th rowspan=\"2\" colspan=\"1\">年度目标</th><th rowspan=\"2\" colspan=\"1\">年度达成率</th></tr>");
+//            sb.append("<tr><th colspan=\"1\">实际</th><th colspan=\"1\">目标</th><th colspan=\"1\">达成率</th><th colspan=\"1\">去年同期</th><th colspan=\"1\">成长率</th>");
+//            sb.append("<th colspan=\"1\">实际</th><th colspan=\"1\">目标</th><th colspan=\"1\">达成率</th><th colspan=\"1\">去年同期</th><th colspan=\"1\">成长率</th>");
+//            sb.append("</tr>");
+//            salesOrder = new SalesOrderAmount();
+//
+//            indicators.clear();
+//            indicators = indicatorBean.findByCategoryAndYear("越南订单金额", y);
+//            indicatorBean.getEntityManager().clear();
+//            indicators.stream().forEach((i) -> {
+//                indicatorBean.divideByRate(i, 2);
+//            });
+//            getHtmlTable(indicators, y, m, d, true);
+//            total = getSumIndicator();
+//            total.setName("越南订单金额");
+//            sb.append(getHtmlTableRow(total, y, m, d));
+//
+//            sb.append("</table></div>");
+//        } catch (Exception ex) {
+//            return ex.toString();
+//        }
+//        return sb.toString();
+//    }
 
 }
