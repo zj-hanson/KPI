185a02fffea9b8db47621e0c269708da6db0f0ce
C1879 <C1879@hanbell.cn>
2018-12-03 19:34:27 +0800

    品保部方型圆型加工合格率表添加

---
+++ KPI-ejb/src/java/cn/hanbell/kpi/evaluation/QRAqualifiedQuantitySumYX.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cn.hanbell.kpi.evaluation;
+
+import cn.hanbell.kpi.ejb.IndicatorBean;
+import cn.hanbell.kpi.entity.Indicator;
+import cn.hanbell.kpi.entity.IndicatorDetail;
+import java.lang.reflect.Field;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+/**
+ *
+ * @author C1879
+ */
+public class QRAqualifiedQuantitySumYX extends QRAqualified{
+
+    IndicatorBean indicatorBean = lookupIndicatorBeanBean();
+
+    public QRAqualifiedQuantitySumYX() {
+        super();
+        queryParams.put("formid", "R-圆型加工");
+        queryParams.put("deptno", "1P000");
+    }
+
+    //圆型件加工总数
+    @Override
+    public BigDecimal getValue(int y, int m, Date d, int type, LinkedHashMap<String, Object> map) {
+        String mon;
+        Field f;
+        BigDecimal v1;
+        Double a1;
+        Indicator i = indicatorBean.findByFormidYearAndDeptno(map.get("formid").toString(), y, map.get("deptno").toString());
+        IndicatorDetail o1 = i.getOther2Indicator();
+        try {
+            mon = indicatorBean.getIndicatorColumn("N", m);
+            f = o1.getClass().getDeclaredField(mon);
+            f.setAccessible(true);
+            a1 = Double.valueOf(f.get(o1).toString());
+            
+            v1 = BigDecimal.valueOf(a1);
+
+            return v1;
+        } catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException ex) {
+            Logger.getLogger(FreeServiceAllSum1G.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return BigDecimal.ZERO;
+    }
+
+    private IndicatorBean lookupIndicatorBeanBean() {
+        try {
+            Context c = new InitialContext();
+            return (IndicatorBean) c.lookup("java:global/KPI/KPI-ejb/IndicatorBean!cn.hanbell.kpi.ejb.IndicatorBean");
+        } catch (NamingException ne) {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, "exception caught", ne);
+            throw new RuntimeException(ne);
+        }
+    }
+}
