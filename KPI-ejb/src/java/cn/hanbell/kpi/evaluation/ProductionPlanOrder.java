d6232c792383484db963c638e4f5dd852e3acc9b
C1879 <C1879@hanbell.cn>
2019-04-13 17:01:26 +0800

    解决每日生产状况表订单部分数据问题

--- KPI-ejb/src/java/cn/hanbell/kpi/evaluation/ProductionPlanOrder.java
+++ KPI-ejb/src/java/cn/hanbell/kpi/evaluation/ProductionPlanOrder.java
@@ -31,6 +31,7 @@ public class ProductionPlanOrder extends Production {
         String itnbrf = map.get("itnbrf") != null ? map.get("itnbrf").toString() : "";
 
         BigDecimal value = BigDecimal.ZERO;
+        BigDecimal value98 = BigDecimal.ZERO;
 
         StringBuilder sb = new StringBuilder();
         sb.append(" select isnull(sum(d.cdrqy1),0) as totcdrqy from cdrhmas h, cdrdmas d ");
@@ -57,18 +58,41 @@ public class ProductionPlanOrder extends Production {
             default:
                 sb.append(" and h.recdate<= '${d}' ");
         }
-        String sql = sb.toString().replace("${y}", String.valueOf(y)).replace("${m}", String.valueOf(m)).replace("${d}", BaseLib.formatDate("yyyyMMdd", d))
+        String sql1 = sb.toString().replace("${y}", String.valueOf(y)).replace("${m}", String.valueOf(m)).replace("${d}", BaseLib.formatDate("yyyyMMdd", d))
+                .replace("${facno}", facno);
+
+        sb.setLength(0);
+        //本月当天人工结案订单
+        sb.append(" select isnull(sum(d.cdrqy1),0) as totcdrqy from cdrhmas h, cdrdmas d ");
+        sb.append(" where  h.hrecsta<>'W' and h.cdrno=d.cdrno and  h.facno=d.facno  and h.facno='${facno}' and d.drecsta IN ('99','98') ");
+        if (!"".equals(n_code_DA)) {
+            sb.append(" and d.n_code_DA ").append(n_code_DA);
+        }
+        if (!"".equals(n_code_DC)) {
+            sb.append(" and d.n_code_DC ").append(n_code_DC);
+        }
+        if (!"".equals(itcls)) {
+            sb.append(" and d.itnbr in(select itnbr from invmas where itcls ").append(itcls).append(") ");
+        }
+        sb.append(" and year(h.recdate) = ${y} and month(h.recdate)= ${m} and h.recdate < '${d}' and left(convert(char(12),d.enddate,112),8)= '${d}'");
+
+        String sql2 = sb.toString().replace("${y}", String.valueOf(y)).replace("${m}", String.valueOf(m)).replace("${d}", BaseLib.formatDate("yyyyMMdd", d))
                 .replace("${facno}", facno);
 
         superEJB.setCompany(facno);
-        Query query = superEJB.getEntityManager().createNativeQuery(sql);
+        Query query1 = superEJB.getEntityManager().createNativeQuery(sql1);
+        Query query2 = superEJB.getEntityManager().createNativeQuery(sql2);
         try {
-            Object o = query.getSingleResult();
-            value = (BigDecimal) o;
+            Object o1 = query1.getSingleResult();
+            Object o2 = query2.getSingleResult();
+            value = (BigDecimal) o1;
+            value98 = (BigDecimal) o2;
+            //减去本月当天发生的人工结案订单
+            return value.subtract(value98);
         } catch (Exception e) {
             Logger.getLogger(Shipment.class.getName()).log(Level.SEVERE, null, e);
         }
-        return value;
+        return BigDecimal.ZERO;
     }
 
 }
