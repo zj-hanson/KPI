939ded78e612cb091307b8bcbaa57bb17b00f87f
C1879 <40564060+C1879@users.noreply.github.com>
2020-02-25 15:45:53 +0800

    螺杆机组内外销计算接口修改、新增

--- KPI-ejb/src/java/cn/hanbell/kpi/evaluation/SalesOrderAmountKWC2.java
+++ KPI-ejb/src/java/cn/hanbell/kpi/evaluation/SalesOrderAmountKWC2.java
@@ -9,17 +9,17 @@ package cn.hanbell.kpi.evaluation;
  *
  * @author C1879
  */
-public class SalesOrderAmountKWC1 extends SalesOrderAmount{
+public class SalesOrderAmountKWC2 extends SalesOrderAmount{
 
-    public SalesOrderAmountKWC1() {
+    public SalesOrderAmountKWC2() {
         super();
         queryParams.put("facno", "K");
-        queryParams.put("deptno", " '5B000'");
+        queryParams.put("deptno", " '5C000'");
         queryParams.put("ogdkid", "RL01");
-        queryParams.put("n_code_DA", " ='OH' ");
-        queryParams.put("n_code_CD", " NOT LIKE 'WX%' ");
+        queryParams.put("n_code_DA", " ='RT' ");
+        queryParams.put("n_code_CD", " LIKE 'WX%' ");
         queryParams.put("n_code_DC", " ='WC' ");
-        queryParams.put("n_code_DD", " In ('00','02') ");
+        queryParams.put("n_code_DD", " In ('00') ");
     }
 
 }
