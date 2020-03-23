82e274840b71af2dff7f2e1d3bca3a2d89914148
C1879 <40564060+C1879@users.noreply.github.com>
2020-03-23 13:14:45 +0800

    A机体+PR600机体制令总和

---
+++ KPI-ejb/src/java/cn/hanbell/kpi/evaluation/ProductionQuantityAH1.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cn.hanbell.kpi.evaluation;
+
+/**
+ *
+ * @author C1879
+ */
+public class ProductionQuantityAH1 extends ProductionQuantity {
+
+    /**
+     * A机体
+     */
+    public ProductionQuantityAH1() {
+        super();
+        //*公司别
+        queryParams.put("facno", "C");
+        //*生产地
+        queryParams.put("prono", "1");
+        //*生产线别
+        queryParams.put("linecode", " = 'AH' ");
+        //制令等级
+        queryParams.put("typecode", "= '01' ");
+        //品号大类
+        queryParams.put("itcls", " NOT IN ('3476','3479','3480') ");
+        
+    }
+}
