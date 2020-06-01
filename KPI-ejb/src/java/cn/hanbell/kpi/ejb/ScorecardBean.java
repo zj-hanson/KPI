8d2271286ff7c1672fb6fbb0ff8b59969daee164
DongTianyu <dtianyu@hanbell.cn>
2020-06-01 13:40:06 +0800

    Update ScorecardBean.java

--- KPI-ejb/src/java/cn/hanbell/kpi/ejb/ScorecardBean.java
+++ KPI-ejb/src/java/cn/hanbell/kpi/ejb/ScorecardBean.java
@@ -7,8 +7,14 @@ package cn.hanbell.kpi.ejb;
 
 import cn.hanbell.kpi.comm.SuperEJBForKPI;
 import cn.hanbell.kpi.entity.Scorecard;
+import cn.hanbell.kpi.entity.ScorecardContent;
+import java.lang.reflect.Field;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.ejb.LocalBean;
+import javax.persistence.Query;
 
 /**
  *
@@ -20,6 +26,50 @@ public class ScorecardBean extends SuperEJBForKPI<Scorecard> {
 
     public ScorecardBean() {
         super(Scorecard.class);
+    }
+
+    public List<Scorecard> findByMenuAndYear(String value, int y) {
+        Query query = getEntityManager().createNamedQuery("Scorecard.findByMenuAndYear");
+        query.setParameter("menu", value);
+        query.setParameter("seq", y);
+        try {
+            return query.getResultList();
+        } catch (Exception ex) {
+            return null;
+        }
+    }
+
+    public String getColumn(String type, int i) {
+        return type.toLowerCase() + String.format("%01d", i);
+    }
+
+    public BigDecimal getScore(List<ScorecardContent> detail, String column) throws Exception {
+        BigDecimal weight;
+        BigDecimal score;
+        BigDecimal total = BigDecimal.ZERO;
+        Object value;
+        Field f;
+        try {
+            for (ScorecardContent entity : detail) {
+                f = entity.getClass().getDeclaredField(column);
+                f.setAccessible(true);
+                value = f.get(entity);
+                if (value == null || "".equals(value)) {
+                    continue;
+                }
+                if (entity.getWeight() == 0) {
+                    total = total.add(BigDecimal.valueOf(Double.valueOf(value.toString())));
+                } else {
+                    weight = BigDecimal.valueOf(entity.getWeight()).divide(BigDecimal.valueOf(100), 2, RoundingMode.HALF_UP);
+                    score = BigDecimal.valueOf(Double.valueOf(value.toString()));
+                    total = total.add(score.multiply(weight));
+                }
+            }
+        } catch (IllegalAccessException | IllegalArgumentException | NoSuchFieldException | SecurityException ex) {
+            log4j.error(ex);
+            throw ex;
+        }
+        return total;
     }
 
 }
