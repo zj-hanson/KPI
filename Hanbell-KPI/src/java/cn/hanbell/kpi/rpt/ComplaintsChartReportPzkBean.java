f826a34517ad04d82c02dda2ad6f0338db5e54b2
C1749 <C1749@hanbell.com.cn>
2021-03-11 11:54:07 +0800

    Update
    品保真空客诉格式调整

--- Hanbell-KPI/src/java/cn/hanbell/kpi/rpt/ComplaintsChartReportPzkBean.java
+++ Hanbell-KPI/src/java/cn/hanbell/kpi/rpt/ComplaintsChartReportPzkBean.java
@@ -116,63 +116,113 @@ public class ComplaintsChartReportPzkBean extends BscChartManagedBean {
         }
 
         chartModel = new LineChartModel();
-        ChartSeries t = new ChartSeries();
-        t.setLabel("客诉比率");
+        ChartSeries a = new ChartSeries();
+        a.setLabel("实际值");
         switch (getIndicator().getFormkind()) {
             case "M":
                 if (getIndicator().getActualIndicator().getN01().compareTo(BigDecimal.ZERO) != 0) {
-                    t.set("M01", getIndicator().getActualIndicator().getN01().doubleValue());
+                    a.set("M01", getIndicator().getActualIndicator().getN01().doubleValue());
                 }
                 if (getIndicator().getActualIndicator().getN02().compareTo(BigDecimal.ZERO) != 0) {
-                    t.set("M02", getIndicator().getActualIndicator().getN02().doubleValue());
+                    a.set("M02", getIndicator().getActualIndicator().getN02().doubleValue());
                 }
                 if (getIndicator().getActualIndicator().getN03().compareTo(BigDecimal.ZERO) != 0) {
-                    t.set("M03", getIndicator().getActualIndicator().getN03().doubleValue());
+                    a.set("M03", getIndicator().getActualIndicator().getN03().doubleValue());
                 }
                 if (getIndicator().getActualIndicator().getN04().compareTo(BigDecimal.ZERO) != 0) {
-                    t.set("M04", getIndicator().getActualIndicator().getN04().doubleValue());
+                    a.set("M04", getIndicator().getActualIndicator().getN04().doubleValue());
                 }
                 if (getIndicator().getActualIndicator().getN05().compareTo(BigDecimal.ZERO) != 0) {
-                    t.set("M05", getIndicator().getActualIndicator().getN05().doubleValue());
+                    a.set("M05", getIndicator().getActualIndicator().getN05().doubleValue());
                 }
                 if (getIndicator().getActualIndicator().getN06().compareTo(BigDecimal.ZERO) != 0) {
-                    t.set("M06", getIndicator().getActualIndicator().getN06().doubleValue());
+                    a.set("M06", getIndicator().getActualIndicator().getN06().doubleValue());
                 }
                 if (getIndicator().getActualIndicator().getN07().compareTo(BigDecimal.ZERO) != 0) {
-                    t.set("M07", getIndicator().getActualIndicator().getN07().doubleValue());
+                    a.set("M07", getIndicator().getActualIndicator().getN07().doubleValue());
                 }
                 if (getIndicator().getActualIndicator().getN08().compareTo(BigDecimal.ZERO) != 0) {
-                    t.set("M08", getIndicator().getActualIndicator().getN08().doubleValue());
+                    a.set("M08", getIndicator().getActualIndicator().getN08().doubleValue());
                 }
                 if (getIndicator().getActualIndicator().getN09().compareTo(BigDecimal.ZERO) != 0) {
-                    t.set("M09", getIndicator().getActualIndicator().getN09().doubleValue());
+                    a.set("M09", getIndicator().getActualIndicator().getN09().doubleValue());
                 }
                 if (getIndicator().getActualIndicator().getN10().compareTo(BigDecimal.ZERO) != 0) {
-                    t.set("M10", getIndicator().getActualIndicator().getN10().doubleValue());
+                    a.set("M10", getIndicator().getActualIndicator().getN10().doubleValue());
                 }
                 if (getIndicator().getActualIndicator().getN11().compareTo(BigDecimal.ZERO) != 0) {
-                    t.set("M11", getIndicator().getActualIndicator().getN11().doubleValue());
+                    a.set("M11", getIndicator().getActualIndicator().getN11().doubleValue());
                 }
                 if (getIndicator().getActualIndicator().getN12().compareTo(BigDecimal.ZERO) != 0) {
-                    t.set("M12", getIndicator().getActualIndicator().getN12().doubleValue());
+                    a.set("M12", getIndicator().getActualIndicator().getN12().doubleValue());
                 }
                 break;
             case "Q":
                 if (getIndicator().getActualIndicator().getNq1().compareTo(BigDecimal.ZERO) != 0) {
-                    t.set("Q1", getIndicator().getActualIndicator().getNq1().doubleValue());
+                    a.set("Q1", getIndicator().getActualIndicator().getNq1().doubleValue());
                 }
                 if (getIndicator().getActualIndicator().getNq2().compareTo(BigDecimal.ZERO) != 0) {
-                    t.set("Q2", getIndicator().getActualIndicator().getNq2().doubleValue());
+                    a.set("Q2", getIndicator().getActualIndicator().getNq2().doubleValue());
                 }
                 if (getIndicator().getActualIndicator().getNq3().compareTo(BigDecimal.ZERO) != 0) {
-                    t.set("Q3", getIndicator().getActualIndicator().getNq3().doubleValue());
+                    a.set("Q3", getIndicator().getActualIndicator().getNq3().doubleValue());
                 }
                 if (getIndicator().getActualIndicator().getNq4().compareTo(BigDecimal.ZERO) != 0) {
-                    t.set("Q4", getIndicator().getActualIndicator().getNq4().doubleValue());
+                    a.set("Q4", getIndicator().getActualIndicator().getNq4().doubleValue());
                 }
                 break;
         }
-        getChartModel().addSeries(t);//比率
+        ChartSeries t = new ChartSeries();
+        t.setLabel("目标值");
+        switch (getIndicator().getFormkind()) {
+            case "M":
+                t.set("M01", getIndicator().getTargetIndicator().getN01().doubleValue());
+                t.set("M02", getIndicator().getTargetIndicator().getN02().doubleValue());
+                t.set("M03", getIndicator().getTargetIndicator().getN03().doubleValue());
+                t.set("M04", getIndicator().getTargetIndicator().getN04().doubleValue());
+                t.set("M05", getIndicator().getTargetIndicator().getN05().doubleValue());
+                t.set("M06", getIndicator().getTargetIndicator().getN06().doubleValue());
+                t.set("M07", getIndicator().getTargetIndicator().getN07().doubleValue());
+                t.set("M08", getIndicator().getTargetIndicator().getN08().doubleValue());
+                t.set("M09", getIndicator().getTargetIndicator().getN09().doubleValue());
+                t.set("M10", getIndicator().getTargetIndicator().getN10().doubleValue());
+                t.set("M11", getIndicator().getTargetIndicator().getN11().doubleValue());
+                t.set("M12", getIndicator().getTargetIndicator().getN12().doubleValue());
+                break;
+            case "Q":
+                t.set("Q1", getIndicator().getTargetIndicator().getNq1().doubleValue());
+                t.set("Q2", getIndicator().getTargetIndicator().getNq2().doubleValue());
+                t.set("Q3", getIndicator().getTargetIndicator().getNq3().doubleValue());
+                t.set("Q4", getIndicator().getTargetIndicator().getNq4().doubleValue());
+                break;
+        }
+        ChartSeries b = new ChartSeries();
+        b.setLabel("基准值");
+        switch (getIndicator().getFormkind()) {
+            case "M":
+                b.set("M01", getIndicator().getBenchmarkIndicator().getN01().doubleValue());
+                b.set("M02", getIndicator().getBenchmarkIndicator().getN02().doubleValue());
+                b.set("M03", getIndicator().getBenchmarkIndicator().getN03().doubleValue());
+                b.set("M04", getIndicator().getBenchmarkIndicator().getN04().doubleValue());
+                b.set("M05", getIndicator().getBenchmarkIndicator().getN05().doubleValue());
+                b.set("M06", getIndicator().getBenchmarkIndicator().getN06().doubleValue());
+                b.set("M07", getIndicator().getBenchmarkIndicator().getN07().doubleValue());
+                b.set("M08", getIndicator().getBenchmarkIndicator().getN08().doubleValue());
+                b.set("M09", getIndicator().getBenchmarkIndicator().getN09().doubleValue());
+                b.set("M10", getIndicator().getBenchmarkIndicator().getN10().doubleValue());
+                b.set("M11", getIndicator().getBenchmarkIndicator().getN11().doubleValue());
+                b.set("M12", getIndicator().getBenchmarkIndicator().getN12().doubleValue());
+                break;
+            case "Q":
+                b.set("Q1", getIndicator().getBenchmarkIndicator().getNq1().doubleValue());
+                b.set("Q2", getIndicator().getBenchmarkIndicator().getNq2().doubleValue());
+                b.set("Q3", getIndicator().getBenchmarkIndicator().getNq3().doubleValue());
+                b.set("Q4", getIndicator().getBenchmarkIndicator().getNq4().doubleValue());
+                break;
+        }
+        getChartModel().addSeries(a);//实际值
+        getChartModel().addSeries(t);//目标值
+        getChartModel().addSeries(b);//基准值
         getChartModel().setTitle(getIndicator().getName());
         getChartModel().setLegendPosition("e");
         getChartModel().setShowPointLabels(true);
