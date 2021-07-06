08d3bb165e4ceb3174bc8e35c64c6512676af202
C0160 <dtianyu@hanbell.cn>
2021-07-06 16:29:03 +0800

    Update CastingProductionMailBean.java

--- KPI-ejb/src/java/cn/hanson/kpi/mail/CastingProductionMailBean.java
+++ KPI-ejb/src/java/cn/hanson/kpi/mail/CastingProductionMailBean.java
@@ -9,6 +9,7 @@ import cn.hanbell.kpi.comm.Actual;
 import cn.hanbell.kpi.comm.MailNotification;
 import cn.hanbell.kpi.entity.Indicator;
 import cn.hanbell.kpi.entity.IndicatorDetail;
+import com.lightshell.comm.BaseLib;
 import java.lang.reflect.Field;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
@@ -185,6 +186,21 @@ public class CastingProductionMailBean extends MailNotification {
         sb.append("<div class=\"tableTitle\">年累计实际: 累计至报表查询日的实际生产数量</div>");
         sb.append("<div class=\"tableTitle\">年累计目标: 之前月份的累计目标 + 本月目标/本月天数 ×当前天数</div>");
         sb.append("<div class=\"tableTitle\">年累计达成: (年累计实际/年累计目标) ×100% </div>");
+        return sb.toString();
+    }
+
+    @Override
+    protected String getMailHead() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("<html><head><title>Hanson</title>");
+        sb.append(css);
+        sb.append("</head><body><div style=\"margin: auto;text-align: center;\">");
+        sb.append("<div style=\"width:100%\" class=\"title\">");
+        sb.append("<div style=\"text-align:center;width:100%\">浙江汉声精密机械有限公司</div>");
+        sb.append("<div style=\"text-align:center;width:100%\">").append(mailSubject).append("</div>");
+        sb.append("<div style=\"text-align:center;width:100%; color:Red;\">日期:")
+            .append(BaseLib.formatDate("yyyy-MM-dd", d)).append("</div>");
+        sb.append("</div>");
         return sb.toString();
     }
 
