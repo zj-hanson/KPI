4112098a08722afa610d3e5a90975dddf9dd04e5
C2082 <shenxin@hanebll.com.cn>
2023-07-05 09:52:40 +0800

    实际值excel导入更新上线

--- Hanbell-KPI/src/java/cn/hanbell/kpi/rpt/PolicySheetReportBean.java
+++ Hanbell-KPI/src/java/cn/hanbell/kpi/rpt/PolicySheetReportBean.java
@@ -102,7 +102,7 @@ public class PolicySheetReportBean extends SuperQueryBean<PolicyDetail> {
         if (a > b) {
             return 1;
         } else if (a == b) {
-            return 1;
+            return 0;
         } else {
             return -1;
         }
